17af9b582017f07c52fa5eca31537590f7650c5d
lokideos <archmage09@gmail.com>
2019-06-24 23:07:51 +0300

    Move env logic to config file

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,11 @@
 
 require_relative '../lib/holy_rider'
 
+unless ENV['RACK_ENV'] == 'production'
+  dotenv = ".env.#{ENV['RACK_ENV']}"
+  raise "#{dotenv} not found! #{dotenv} is required to start in #{ENV['RACK_ENV']} mode!" unless File.exist?(dotenv)
+
+  Dotenv.load(dotenv)
+end
+
 HolyRider.application.bootstrap!
--- lib/holy_rider/application.rb
+++ lib/holy_rider/application.rb
@@ -7,14 +7,6 @@ module HolyRider
   class Application < Roda
     def self.bootstrap!
       # setup database
-
-      return if ENV['RACK_ENV'] == 'production'
-
-      dotenv = ".env.#{ENV['RACK_ENV']}"
-
-      raise "#{dotenv} not found! #{dotenv} is required to start in #{ENV['RACK_ENV']} mode!" unless File.exist?(dotenv)
-
-      Dotenv.load(dotenv)
     end
   end
 end

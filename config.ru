9528001759a7cf3ef063de04f3e1cb9c283241fc
lokideos <archmage09@gmail.com>
2019-06-24 22:41:08 +0300

    Add rack unreloader to the project; load env variables

--- config.ru
+++ config.ru
@@ -1,3 +1,11 @@
 require_relative "config/environment"
 
-run HolyRider.freeze.application
+if ENV['RACK_ENV'] == 'development'
+  require 'rack/unreloader'
+  unreloader = Rack::Unreloader.new{HolyRider}
+  unreloader.require '.config/'
+  Dir.glob('lib/**/*.rb').each { |file_name| unreloader.require(file_name) }
+  run unreloader
+else
+  run HolyRider.application
+end
--- lib/holy_rider/application.rb
+++ lib/holy_rider/application.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
 require 'roda'
+require 'dotenv'
 
 module HolyRider
   class Application < Roda
     def self.bootstrap!
       # setup database
+
+      return if ENV['RACK_ENV'] == 'production'
+
+      dotenv = ".env.#{ENV['RACK_ENV']}"
+
+      raise "#{dotenv} not found! #{dotenv} is required to start in #{ENV['RACK_ENV']} mode!" unless File.exist?(dotenv)
+
+      Dotenv.load(dotenv)
     end
   end
 end

95437f03498586f64102a8b455a9bcaef3766354
lokideos <archmage09@gmail.com>
2019-11-28 09:32:14 +0300

    Add roda routes to test Telegram bot functionality

--- lib/holy_rider/roda_tree.rb
+++ lib/holy_rider/roda_tree.rb
@@ -16,6 +16,15 @@ module HolyRider
       r.on 'favicon.ico' do
         '?'
       end
+
+      # TODO: delete telegram routes before release
+      r.on 'get_updates' do
+        HolyRider::Service::ChatUpdateService.new.call.to_json
+      end
+
+      r.on 'send_message' do
+        HolyRider::Service::SendChatMessageService.new(chat_id: ENV['PS_CHAT_ID'], message: 'Test').call.to_json
+      end
     end
   end
 end

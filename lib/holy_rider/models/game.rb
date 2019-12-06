f5497b6c6ea86b9886978cdf24238059605561a6
lokideos <archmage09@gmail.com>
2019-12-06 14:53:14 +0300

    Create trophies and link them to players and games

--- lib/holy_rider/models/game.rb
+++ lib/holy_rider/models/game.rb
@@ -4,5 +4,6 @@ class Game < Sequel::Model
   Game.plugin :timestamps, update_on_create: true
 
   one_to_many :game_acquisitions
+  one_to_many :trophies
   many_to_many :players, left_key: :game_id, right_key: :player_id, join_table: :game_acquisitions
 end
--- lib/holy_rider/models/player.rb
+++ lib/holy_rider/models/player.rb
@@ -4,7 +4,10 @@ class Player < Sequel::Model
   Player.plugin :timestamps, update_on_create: true
 
   one_to_many :game_acquisitions
+  one_to_many :trophy_acquisitions
   many_to_many :games, left_key: :player_id, right_key: :game_id, join_table: :game_acquisitions
+  many_to_many :trophies, left_key: :player_id, right_key: :trophy_id,
+                          join_table: :trophy_acquisitions
 
   dataset_module do
     def active_trophy_accounts

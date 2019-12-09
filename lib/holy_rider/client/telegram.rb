e7550fe3d9541679226b67c98f59f2b1fcf3e629
lokideos <archmage09@gmail.com>
2019-12-09 22:42:42 +0300

    Fix telegram updates reciever

--- lib/holy_rider/client/telegram.rb
+++ lib/holy_rider/client/telegram.rb
@@ -17,10 +17,18 @@ module HolyRider
 
       def initialize
         @chat_bot = ::Telegram.bots[:chat]
+        @redis = HolyRider::Application.instance.redis
       end
 
       def get_updates
-        chat_bot.get_updates
+        offset_value = @redis.get('holy_rider:telegram:offset')
+        updates = chat_bot.get_updates(offset: offset_value || '')
+        unless updates['result'].empty?
+          last_update_id = chat_bot.get_updates['result'].last['update_id']
+          @redis.set('holy_rider:telegram:offset', last_update_id)
+        end
+
+        updates
       end
 
       def send_message(chat_id:, message:)

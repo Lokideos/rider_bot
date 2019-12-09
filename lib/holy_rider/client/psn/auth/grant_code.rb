4bdbd964cb4eafb904e41f876787c58813d0d4ab
lokideos <archmage09@gmail.com>
2019-12-09 22:41:51 +0300

    Correct headers for psn requests

--- lib/holy_rider/client/psn/auth/grant_code.rb
+++ lib/holy_rider/client/psn/auth/grant_code.rb
@@ -29,9 +29,9 @@ module HolyRider
 
           def headers(sso_cookie)
             {
-              'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) ' \
+              'User-Agent' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) ' \
                 'AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
-              'Cookie': "npsso=#{sso_cookie}"
+              'Cookie' => "npsso=#{sso_cookie}"
             }
           end
 
--- lib/holy_rider/client/psn/trophy/all_trophy_titles.rb
+++ lib/holy_rider/client/psn/trophy/all_trophy_titles.rb
@@ -37,9 +37,9 @@ module HolyRider
 
           def headers
             {
-              'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) ' \
+              'User-Agent' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) ' \
                 'AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
-              'Authorization': "Bearer #{@token}"
+              'Authorization' => "Bearer #{@token}"
             }
           end
 

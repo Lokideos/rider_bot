5e13edde68693d7c7665a02fda6156fa5c041ada
George <archmage09@gmail.com>
2019-12-18 01:34:21 +0300

    170314485 | Add unfollow player command for admins (#34)
    * Add commands to place player on and off trophy watch
    
    * Update help

---
+++ lib/holy_rider/service/command/player_watch_on.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module HolyRider
+  module Service
+    module Command
+      class PlayerWatchOn
+        def initialize(command, message_type)
+          @command = command
+          @message_type = message_type
+          @redis = HolyRider::Application.instance.redis
+        end
+
+        def call
+          player_name = @command[@message_type]['text'].split(' ')[1]
+          player = Player.find(telegram_username: player_name)
+          message = ['Трофеи игрока уже отслеживаются']
+          return message if player.on_watch?
+
+          player.update(on_watch: true)
+          @redis.sadd('holy_rider:watcher:players', player.trophy_account)
+
+          ['Теперь трофеи игрока отслеживаются']
+        end
+      end
+    end
+  end
+end

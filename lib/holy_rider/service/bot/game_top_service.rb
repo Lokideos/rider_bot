1bdec2afa95628d230142e1586955807efcb40b4
George <archmage09@gmail.com>
2019-12-19 22:34:40 +0300

    170366504 | Apply order to game trophy top (#41)
    * Restructure game top
    
    * Fix games top info
    
    * Fix tlegram forming messages
    
    * Fix find command

--- lib/holy_rider/service/bot/game_top_service.rb
+++ lib/holy_rider/service/bot/game_top_service.rb
@@ -14,15 +14,10 @@ module HolyRider
         # TODO: Refactoring needed
         def call
           game_top = []
-          @top[:progresses].each do |progress|
-            player_name = progress[:trophy_account]
-            player_progress = progress[:progress]
-            platinum_trophy = nil
-            if @top[:platinum] && !@top[:platinum].empty?
-              if @top[:platinum].find { |trophy| trophy.trophy_account == player_name }
-                platinum_trophy = "\xF0\x9F\x8F\x86"
-              end
-            end
+          @top.each do |progress|
+            player_name = progress.trophy_account
+            player_progress = progress.progress
+            platinum_trophy = progress.platinum_earning_date ? "\xF0\x9F\x8F\x86" : nil
             player_name = player_name[0..11] + '...' if player_name.length > 12
             game_top << "<code>#{player_name} " + ' ' * (MAX_NAME_LENGTH - player_name.length) +
                         player_progress.to_s +

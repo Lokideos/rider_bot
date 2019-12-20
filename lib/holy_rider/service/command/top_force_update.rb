7fd31a4b0038181cbd9dcbbbaf417d5dcf04970b
lokideos <archmage09@gmail.com>
2019-12-21 01:14:42 +0300

    Update top force update admin command

--- lib/holy_rider/service/command/top_force_update.rb
+++ lib/holy_rider/service/command/top_force_update.rb
@@ -13,16 +13,8 @@ module HolyRider
           player = Player.find(telegram_username: @command[@message_type]['from']['username'])
           return unless player.admin?
 
-          updated_top = Player.trophy_top_force_update
-          message = ['Топ трофеев был успешно обновлен']
-          message << 'Обновленный топ трофеев:'
-          updated_top.each do |player_trophies|
-            max_name_length = 15
-            name = player_trophies[:trophy_account] || player_trophies[:telegram_username]
-            name = name[0..11] + '...' if name.length > 12
-            message << "<code>#{name}" + ' ' * (max_name_length - name.length) +
-                       " #{player_trophies[:points]}</code>"
-          end
+          Player.trophy_top_force_update
+          message = ['Топы трофеев были успешно обновлены']
 
           [message.join("\n")]
         end

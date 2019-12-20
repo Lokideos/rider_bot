15925c95941a7f3f234ef7a5d597fd400a48f906
George <archmage09@gmail.com>
2019-12-21 00:33:41 +0300

    170409815 | Refactoring me stats commands (#47)
    * Refactoring Me command
    
    * Refactoring stats command

--- lib/holy_rider/service/command/me.rb
+++ lib/holy_rider/service/command/me.rb
@@ -12,24 +12,31 @@ module HolyRider
         def call
           player = Player.find(telegram_username: @command['message']['from']['username'])
           profile = player.profile
+          player_name = player.telegram_username
           trophy_account = player.trophy_account
           telegram_name = player.telegram_username
+          trophy_total = profile[:trophies][:total].count + profile[:hidden_trophies][:total].count
           message = ["<b>Профиль игрока #{trophy_account} (@#{telegram_name})</b>"]
           message << "<code>PSN level         #{profile[:trophy_level]}</code>"
           message << "<code>Level up progress #{profile[:level_up_progress]}</code>"
           message << "<code>Количество игр    #{profile[:games].count}</code>"
+          message << "<code>Всего трофеев     #{trophy_total}</code>"
           message << "<b>\nТрофеи:</b>"
-          message << "<code>Платиновые трофеи #{profile[:trophies][:platinum].count}</code>"
-          message << "<code>Золотые трофеи    #{profile[:trophies][:gold].count}</code>"
-          message << "<code>Серебряные трофеи #{profile[:trophies][:silver].count}</code>"
-          message << "<code>Бронзовые трофеи  #{profile[:trophies][:bronze].count}</code>"
-          message << "<code>Всего трофеев     #{profile[:trophies][:total].count}</code>"
-          message << "\n<b>Скрытые трофеи:</b>"
-          message << "<code>Платиновые трофеи #{profile[:hidden_trophies][:platinum].count}</code>"
-          message << "<code>Золотые трофеи    #{profile[:hidden_trophies][:gold].count}</code>"
-          message << "<code>Серебряные трофеи #{profile[:hidden_trophies][:silver].count}</code>"
-          message << "<code>Бронзовые трофеи  #{profile[:hidden_trophies][:bronze].count}</code>"
-          message << "<code>Всего трофеев     #{profile[:hidden_trophies][:total].count}</code>"
+          message << "<code>Платина           #{profile[:trophies][:platinum].count}</code>"
+          message << "<code>Золото            #{profile[:trophies][:gold].count}</code>"
+          message << "<code>Серебро           #{profile[:trophies][:silver].count}</code>"
+          message << "<code>Бронза            #{profile[:trophies][:bronze].count}</code>"
+          message << "<code>Всего             #{profile[:trophies][:total].count}</code>"
+          if profile[:hidden_trophies][:total].count.zero?
+            message << "\n<b>Скрытые трофеи:</b> #{player_name} скрывать нечего!"
+          else
+            message << "<b>\nСкрытые трофеи:</b>"
+            message << "<code>Платина           #{profile[:hidden_trophies][:platinum].count}</code>"
+            message << "<code>Золото            #{profile[:hidden_trophies][:gold].count}</code>"
+            message << "<code>Серебро           #{profile[:hidden_trophies][:silver].count}</code>"
+            message << "<code>Бронза            #{profile[:hidden_trophies][:bronze].count}</code>"
+            message << "<code>Всего             #{profile[:hidden_trophies][:total].count}</code>"
+          end
 
           [message.join("\n")]
         end

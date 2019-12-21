11dd4dfb6c34318924c1bf8f90186ad3c9891c03
George <archmage09@gmail.com>
2019-12-21 15:59:27 +0300

    170413281 | Adjust trophy top view (#55)
    * Adjust common trophy top view
    
    * Adjust rare trophy top view

--- lib/holy_rider/service/command/top_rare.rb
+++ lib/holy_rider/service/command/top_rare.rb
@@ -4,7 +4,7 @@ module HolyRider
   module Service
     module Command
       class TopRare
-        MAX_NAME_LENGTH = 18
+        MAX_NAME_LENGTH = 15
         MAX_PLACEMENT_LENGTH = 3
 
         def initialize(command, message_type)
@@ -17,7 +17,7 @@ module HolyRider
           message = ['<b>Топ редких трофеев:</b>']
           Player.trophy_top_rare.each_with_index do |player_trophies, index|
             name = player_trophies[:telegram_username]
-            name = name[0..16] + '...' if name.length > 17
+            name = name[0..14] + '...' if name.length > 15
             placement = (index + 1).to_s
             message << "<code>#{placement}" +
                        ' ' * (MAX_PLACEMENT_LENGTH - placement.length) +

ac22094d1ffadf50e948bf9e9efdf7809aab8da8
lokideos <archmage09@gmail.com>
2019-12-23 08:17:15 +0300

    Add command to rename player telegram name by admins

---
+++ lib/holy_rider/service/command/player_rename.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module HolyRider
+  module Service
+    module Command
+      class PlayerRename
+        def initialize(command, message_type)
+          @command = command
+          @message_type = message_type
+          @redis = HolyRider::Application.instance.redis
+        end
+
+        def call
+          player = Player.find(telegram_username: @command[@message_type]['from']['username'])
+          return unless player.admin?
+
+          message = @command[@message_type]['text'].split(' ')
+          username = message[1]
+          player = Player.find(telegram_username: username)
+          return ["Пользователь #{username} не найден"] unless player
+
+          new_username = message[2]
+          return ['Введите корректный никнейм'] unless new_username
+
+          player.update(telegram_username: new_username)
+
+          ["Пользователь #{username} успешно переименован в #{new_username}"]
+        end
+      end
+    end
+  end
+end

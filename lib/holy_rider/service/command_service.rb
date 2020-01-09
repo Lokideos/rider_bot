6411d798a3bd39b24440e8a3a9655c0ff97b95c3
lokideos <archmage09@gmail.com>
2020-01-09 22:12:10 +0300

    Do not allow to use admin commands outside of admins chat

--- lib/holy_rider/service/command_service.rb
+++ lib/holy_rider/service/command_service.rb
@@ -80,6 +80,7 @@ module HolyRider
         @message_type = message_type
       end
 
+      # TODO: combine multiple guard clauses to separate checks in methods
       def call
         return unless @allowed_chat_ids.include? @current_chat_id.to_s
 
@@ -92,6 +93,10 @@ module HolyRider
           command = command.split('@').first
         end
         return unless [COMMON_COMMANDS, ADMIN_COMMANDS].flatten.include? command
+
+        if ADMIN_COMMANDS.include? command
+          return unless @current_chat_id == @admin_chat_id
+        end
 
         command = 'get_game_from_cache' if CACHED_GAMES.include? command
         messages = Kernel.const_get(

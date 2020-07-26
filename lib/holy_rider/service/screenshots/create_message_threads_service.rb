a1d44865ccb8c73d1520577a46a2b360623fd01f
lokideos <archmage09@gmail.com>
2020-07-26 17:54:56 +0300

    check cmt

--- lib/holy_rider/service/screenshots/create_message_threads_service.rb
+++ lib/holy_rider/service/screenshots/create_message_threads_service.rb
@@ -17,6 +17,8 @@ module HolyRider
             }
           end
 
+          binding.pry
+
           prepared_threads.each do |thread|
             player = Player.find(message_thread_name: thread[:message_thread_name])
             player.add_message_thread(

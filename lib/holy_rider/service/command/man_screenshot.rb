02813c6352b220a2096db1318f5729dfcae8fe41
lokideos <archmage09@gmail.com>
2020-07-26 18:17:29 +0300

    Add man_screenshot command

---
+++ lib/holy_rider/service/command/man_screenshot.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module HolyRider
+  module Service
+    module Command
+      class ManScreenshot
+        def initialize(command, message_type)
+          @command = command
+          @message_type = message_type
+        end
+
+        def call
+          message = ['<b>Загрузка скриншота</b>']
+          message << 'Для загрузки скриншота отправьте его psn аккаунту бота.'
+          message << "На данный момент это <code>#{ENV['SCREENSHOT_BOT_NAME']}</code>."
+          message << 'Через какое-то время скриншот появится в чате с указаниям на того, ' \
+                     'кто его отправил.'
+
+          [message.join("\n")]
+        end
+      end
+    end
+  end
+end
